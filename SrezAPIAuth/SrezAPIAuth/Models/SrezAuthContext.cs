b1a169ee2f761d26e456a037a356cbedbaf833bb
Endroshock <dr.xxx-27@yandex.ru>
2022-05-11 22:12:20 +0300

    first commit

---
+++ SrezAPIAuth/SrezAPIAuth/Models/SrezAuthContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SrezAPIAuth.Models
+{
+    public partial class SrezAuthContext : DbContext
+    {
+        public SrezAuthContext()
+        {
+        }
+
+        public SrezAuthContext(DbContextOptions<SrezAuthContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=SrezAuth;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Login)
+                    .HasName("PK__Users__5E55825A82343AF2");
+
+                entity.Property(e => e.Login).HasMaxLength(100);
+
+                entity.Property(e => e.Middlename).HasMaxLength(100);
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.Property(e => e.Password).HasMaxLength(100);
+
+                entity.Property(e => e.Surname).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ SrezAPIAuth/SrezAPIAuth/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SrezAPIAuth.Models
+{
+    public partial class User
+    {
+        public string Login { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Middlename { get; set; } = null!;
+        public byte[]? Photo { get; set; }
+    }
+}
